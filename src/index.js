83deffcc0d25063ebdf7bf24fc6acac769cf29da
babu97 <benkaimugul@gmail.com>
2023-10-09 08:55:44 +0300

    preventDefault assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form")
+  form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    
+  })
 });
